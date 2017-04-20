6c6ae25fc1d3225d768c8f672fdcdbe1c9d0b236
spaceorc <spaceorc@skbkontur.ru>
2017-04-21 00:36:21 +0500

    Virtual damage set to 0

--- Game/Settings.cs
+++ Game/Settings.cs
@@ -10,10 +10,14 @@ namespace Game
 		public const int NAVIGATION_PATH_DEPTH = 5;
 
 		public const int NEAR_ENEMY_SHIP_MIN_DIST = 4;
-		public const int NEAR_ENEMYSHIP_VIRTUAL_DAMAGE = 5;
+		public const int NEAR_ENEMYSHIP_VIRTUAL_DAMAGE = 0;
 
 		public const int FREE_WALK_TARGET_REACH_DIST = 5;
 		
+		public const int CANNONBALLS_FORECAST_TRAVEL_TIME_LIMIT = 1;
+
+		public const int CANNONS_TRAVEL_TIME_LIMIT = 2;
+
 		public const int DUMP_TURN = -1;
 		public const int DUMP_STAT_TURN = -1;
 	}
